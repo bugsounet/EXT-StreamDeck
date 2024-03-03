1478a8ff2e14bbb291109fdce0b317063bb0b48a
bugsounet <bugsounet@bugsounet.fr>
2024-03-03 15:51:06 +0100

    eslint fix

--- EXT-StreamDeck.js
+++ EXT-StreamDeck.js
@@ -1,8 +1,7 @@
-/***********************
-*  EXT-StreamDeck v1.1 *
-*  Bugsounet           *
-*  03/2023             *
-***********************/
+/******************
+*  EXT-StreamDeck *
+*  Bugsounet      *
+*******************/
 
 Module.register("EXT-StreamDeck", {
   defaults: {
@@ -64,62 +63,62 @@ Module.register("EXT-StreamDeck", {
     ]
   },
 
-  start: function() {
-    this.resources = "/modules/EXT-StreamDeck/resources/"
-    this.audio = null
-    this.ready= false
+  start () {
+    this.resources = "/modules/EXT-StreamDeck/resources/";
+    this.audio = null;
+    this.ready= false;
   },
 
-  getDom: function() {
-    var wrapper = document.createElement("div")
-    wrapper.style.display = 'none'
-    return wrapper
+  getDom () {
+    var wrapper = document.createElement("div");
+    wrapper.style.display = "none";
+    return wrapper;
   },
 
-  notificationReceived: function(noti, payload, sender) {
-    if (noti == "GA_READY") {
-      if (sender.name == "MMM-GoogleAssistant") {
-        this.sendSocketNotification("INIT", this.config)
-        this.audio = new Audio()
-        this.audio.autoplay = true
+  notificationReceived (noti, payload, sender) {
+    if (noti === "GA_READY") {
+      if (sender.name === "MMM-GoogleAssistant") {
+        this.sendSocketNotification("INIT", this.config);
+        this.audio = new Audio();
+        this.audio.autoplay = true;
       }
     }
-    if (!this.ready) return
+    if (!this.ready) return;
 
     switch(noti) {
       case "EXT_STREAMDECK-ON":
-        this.sendSocketNotification("ON")
-        break
+        this.sendSocketNotification("ON");
+        break;
       case "EXT_STREAMDECK-OFF":
-        this.sendSocketNotification("OFF")
-        break
+        this.sendSocketNotification("OFF");
+        break;
     }
   },
 
-  socketNotificationReceived: function(noti, payload) {
+  socketNotificationReceived (noti, payload) {
     switch(noti) {
       case "INITIALIZED":
-        this.ready= true
-        this.sendNotification("EXT_HELLO", this.name)
-        break
+        this.ready= true;
+        this.sendNotification("EXT_HELLO", this.name);
+        break;
       case "WARNING":
         this.sendNotification("EXT_ALERT", {
           type: "warning",
           message: payload.message
-        })
-        break
+        });
+        break;
       case "NOTIFICATION":
-        this.sendNotification(payload.notification, payload.payload || undefined)
-        break
+        this.sendNotification(payload.notification, payload.payload || undefined);
+        break;
       case "SOUND":
-        this.audio.src= this.resources + payload + ".mp3"
-        break
+        this.audio.src= `${this.resources + payload}.mp3`;
+        break;
       case "KEYFINDER":
         this.sendNotification("EXT_ALERT", {
           type: "information",
-          message: "You pressed key number: " + payload.key,
+          message: `You pressed key number: ${payload.key}`,
           timer: 3000
-        })
+        });
     }
   }
 });
